57b7f1fd80678ef90adb78a3f6ea3c5387619069
Ubuntu <ubuntu@ip-172-31-22-53.ec2.internal>
2026-07-21 13:24:05 +0000

    Fix production Keycloak HTTPS URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: "http://100.53.168.109:3000",
-  keycloakUrl: "http://100.53.168.109:8080"
+  keycloakUrl: "https://geo-task-app.duckdns.org"
 };
