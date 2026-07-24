aec5fed0ff782127d18d8d7d67af19d28d505ac5
Ubuntu <ubuntu@ip-172-31-22-53.ec2.internal>
2026-07-24 16:40:15 +0000

    Update production environment and deployment configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -52,7 +52,7 @@ export const appConfig: ApplicationConfig = {
     { provide: MapApiProvider, useClass: RestMapApiService },
     provideKeycloak({
       config: {
-        url: 'http://localhost:8080',
+        url: 'https://geo-task-app.duckdns.org',
         realm: 'geo-task-realm',
         clientId: 'geo-task-web-client'
       },
@@ -64,3 +64,4 @@ export const appConfig: ApplicationConfig = {
     })
   ]
 };
+ 
