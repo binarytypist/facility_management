eef9311c220f81c7a2b08254e117ad6e9d43812e
Ubuntu <ubuntu@ip-172-31-22-53.ec2.internal>
2026-07-24 16:39:06 +0000

    Update production environment and deployment configuration

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,7 +1,13 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", ".prisma/client"],
+
+  allowedDevOrigins: [
+    "http://100.53.168.109",
+    "http://100.53.168.109:3000",
+  ],
 };
 
 export default nextConfig;
